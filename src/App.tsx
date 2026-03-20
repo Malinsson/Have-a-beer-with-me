f97f2aeabd682116e6b98b4788f5beecdf4f883d
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-03-20 16:47:05 +0100

    Add router and Layout scaffold to App
    Add react-router imports and a Layout wrapper, and define Routes for '/', '/design', and '/profile/:id'. Remove unused useState import.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import CanPreview3D from './features/can-designer/components/CanPreview3D.tsx'
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.tsx'
 import './App.css'
 
 
 
 function App() {
+
+  <Layout>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/design" element={<DesignBeerCan />}></Route>
+      <Route path="/profile/:id" element={<ProfilePage />}></Route>
+    </Routes>
+  </Layout>
+  
 
   return (
     <>
