730c956f6a545ab192c601350b01f4f1b3eff4b1
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-03-23 13:59:52 +0100

    Add auth/design/beer pages and routes
    Reorganize imports (move App.css, add comment) and register new pages: AuthPage, DesignerPage, BeerShelfPage, and CanDetailPage. Replace the old /edit route with /design, add /auth, /profile/:id/hylla (beer shelf) and /detail routes, and keep Layout wrapping the Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
-import { Layout } from './components/Layout.tsx'
-import { HomePage } from './pages/HomePage.tsx'
-import { EditorPage } from './pages/DesignerPage.tsx'
-import { ProfilePage } from './pages/ProfilePage.tsx'
 import './App.css'
+
+// Pages
+import { Layout } from './components/Layout.tsx' // Importing header and footer
+import { HomePage } from './pages/HomePage.tsx'
+import { AuthPage } from './pages/AuthPage.tsx'
+import { ProfilePage } from './pages/ProfilePage.tsx'
+import { DesignerPage } from './pages/DesignerPage.tsx'
+import { BeerShelfPage } from './pages/BeerShelfPage.tsx'
+import { CanDetailPage } from './pages/CanDetailPage.tsx'
+
 
 
 
@@ -14,8 +20,11 @@ export function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/edit" element={<EditorPage />}></Route>
+          <Route path="/auth" element={<AuthPage />}></Route>
+          <Route path="/design" element={<DesignerPage />}></Route>
           <Route path="/profile/:id" element={<ProfilePage />}></Route>
+          <Route path="/profile/:id/hylla" element={<BeerShelfPage />}></Route>
+          <Route path="/detail" element={<CanDetailPage />}></Route>
         </Routes>
       </Layout>
     
