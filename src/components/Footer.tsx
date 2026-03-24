373cc9ff30991515c8f83d468969db7d8ab648b1
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-03-24 11:24:42 +0100

    Anchor footer and adjust global styles
    Add bottom-0 and w-full to the Footer component to anchor it to the bottom and span full width. Remove the global margin reset (margin: 0) from index.css to stop overriding default margins.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="bg-red-500 p-4 pt-30 mt-8">
+        <footer className="bg-red-500 p-4 pt-30 bottom-0 w-full">
             <p className="text-center text-white">&copy; 2026 Have a Beer with Me. All rights reserved.</p>
         </footer>
     );
