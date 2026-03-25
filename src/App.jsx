0aee0507189d3f61bf935dc445488cd697117f01
Malin Persson <malin.persson02@hotmail.com>
2026-03-25 14:47:42 +0100

    Small fixes to 3d

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { SignupForm } from './features/auth/components/SignupForm';
 import { LoginForm } from './features/auth/components/LoginForm';
 import { ListUsers } from './tests/ListUsers';
 import { ImageUploader } from './features/can-designer/components/ImageUploader';
+import { CanPreview3D } from './features/can-designer/components/CanPreview3D';
 
 
 
@@ -19,6 +20,7 @@ function App() {
       <LoginForm />
       <ListUsers />
       <ImageUploader onUploadComplete={handleUploadComplete} />
+      <CanPreview3D />
     </>
   )
 }
--- src/features/can-designer/components/CanPreview3D.jsx
+++ src/features/can-designer/components/CanPreview3D.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, useTexture, PresentationControls, Stage } from '@react-three/drei'
+import { OrbitControls, Stage, useGLTF, useTexture } from '@react-three/drei'
 import * as THREE from 'three'
 import beerCanSceneUrl from '../../../assets/3DModels/BeerCan3.glb?url'
 import beerCanLabelUrl from '../../../assets/3DModels/BeerCanLabel.png'
@@ -27,17 +27,16 @@ function Model(props) {
   return <primitive object={scene} {...props} />
 }
 
-export default function CanPreview3D() {
+export function CanPreview3D() {
 
   return (
     <>
-      <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 5], fov: 50 }} style={{ position: 'absolute', touchAction: 'none' }}>
+      <Canvas dpr={[1, 2]} gl={{ precision: 'mediump' }} camera={{ position: [0, 0, 5], fov: 50 }} style={{ position: 'absolute', touchAction: 'none' }}>
         <color attach="background" args={['#2b2727']} />
-        <PresentationControls global rotation={[0, Math.PI / 4, 0]} polar={[-0.3, 0.3]} speed={1.5} zoom={0.6}  >
-          <Stage environment="studio" intensity={0.1} shadows={false}>
-            <Model scale={0.005} />
-          </Stage>
-        </PresentationControls>
+        <Stage>
+          <Model scale={0.5} />
+        </Stage>
+        <OrbitControls enablePan={false} minDistance={3} maxDistance={8} />
       </Canvas>
     </>
   )
