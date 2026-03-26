17ba2b607d0657fe72fa8257ad82fc10f8ed827e
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-03-26 15:19:09 +0100

    Align footer logo to right
    Wrap the Bomarke logo image in a right-aligned flex container inside Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ import bomarkeWhiteLogo from '../assets/images/logo/bomarke-white.png';
 export const Footer = () => {
     return (
         <footer className="bg-yrgo-red p-4 pt-30 bottom-0 w-full">
-            <img src={bomarkeWhiteLogo} alt="Bomarke Logo" />
+            <div className='flex justify-end'>
+                <img 
+                    src={bomarkeWhiteLogo} 
+                    alt="Bomarke Logo" 
+                />
+            </div>
         </footer>
     );
 }
