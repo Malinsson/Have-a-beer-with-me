67851d19e3fe045400abd30da214c994f56ab0bb
Malin Persson <malin.persson02@hotmail.com>
2026-04-10 15:44:07 +0200

    Added animation to finish design modal

--- src/shared/components/Modal.jsx
+++ src/shared/components/Modal.jsx
@@ -1,6 +1,6 @@
 import { FaArrowRight } from "react-icons/fa6";
 import { Button } from "./Button";
-import image from '../../assets/images/beer.png';
+import canReadyComp from '../../assets/animations/canReadyComp.mp4';
 
 export const Modal = ({ onConfirm }) => {
     return (
@@ -9,15 +9,21 @@ export const Modal = ({ onConfirm }) => {
             onClick={onConfirm}
         >
             <div
-                className="bg-white border p-4 py-20 mx-6 flex flex-col items-center gap-4 max-w-sm w-full"
+                className="bg-white border p-4 mx-6 flex flex-col max-w-sm w-full h-[70vh] max-h-140 relative"
                 onClick={(e) => e.stopPropagation()}
             >
-                <h3 className="text-2xl text-center text-dark-blue">
+                <h3 className="text-2xl text-center text-dark-blue w-full absolute top-10 left-1/2 -translate-x-1/2">
                     Din burk är färdig!
                 </h3>
 
-                <div className="w-40 h-40">
-                    <img src={image} alt="bomarke logo" />
+                <div className="w-full flex-1 flex items-center justify-center">
+                    <video 
+                        src={canReadyComp} 
+                        alt="Din färdiga burk"
+                        className="max-h-full max-w-full object-contain"
+                        autoPlay
+                        muted
+                    />
                 </div>
                 
                 <Button 
@@ -25,6 +31,7 @@ export const Modal = ({ onConfirm }) => {
                     type="button"
                     onClick={onConfirm}
                     showIcon={true}
+                    className="absolute bottom-6 left-1/2 -translate-x-1/2 max-w-[70vw]"
                 />
             </div>
         </div>
